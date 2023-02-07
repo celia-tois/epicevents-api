77a4cbfef671b2e04c92021f946f12e94a733ae8
Célia Tois <celia.tois@gmail.com>
2023-02-07 19:12:28 +0100

    create the permissions and overwrite the get_queryset function

--- epicevents/application/views.py
+++ epicevents/application/views.py
@@ -2,22 +2,30 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from application.serializers import UserSerializer, ClientSerializer, ContractSerializer, EventSerializer
 from application.models import User, Client, Contract, Event
+from application.permissions import UserPermission, ClientPermission, ContractPermission, EventPermission
 
 
 class UserViewset(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, UserPermission]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class ClientViewset(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, ClientPermission]
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
 
+    def get_queryset(self):
+        if self.request.user.role == 'SUPPORT':
+            return self.queryset.filter(client_event__support_contact=self.request.user)
+        elif self.request.user.role == 'SALES':
+            return self.queryset.filter(client_contract__sales_contact=self.request.user)
+        return self.queryset
+
 
 class ContractViewset(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, ContractPermission]
     queryset = Contract.objects.all()
     serializer_class = ContractSerializer
 
@@ -26,8 +34,18 @@ class ContractViewset(ModelViewSet):
         client = Client.objects.get(id=client_id)
         serializer.save(sales_contact=client.sales_contact)
 
+    def get_queryset(self):
+        if self.request.user.role == 'SALES':
+            return self.queryset.filter(sales_contact=self.request.user)
+        return self.queryset
+
 
 class EventViewset(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, EventPermission]
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+    def get_queryset(self):
+        if self.request.user.role == 'SUPPORT':
+            return self.queryset.filter(support_contact=self.request.user)
+        return self.queryset
