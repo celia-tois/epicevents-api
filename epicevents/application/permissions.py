60be2907fb4e443dae2008ee77fd8b5190982e74
Célia Tois <celia.tois@gmail.com>
2023-02-20 12:12:29 +0100

    add condition to the permission to make them work properly

--- epicevents/application/permissions.py
+++ epicevents/application/permissions.py
@@ -19,7 +19,7 @@ class ClientPermission(BasePermission):
         Allow users in the Sales team to perform a GET and a POST.
         """
         user_role = request.user.role
-        if user_role == 'MANAGEMENT' and request.method == 'GET':
+        if user_role == 'MANAGEMENT' and request.method in ['GET', 'PUT']:
             return True
         if user_role == 'SUPPORT' and request.method == 'GET':
             return True
@@ -40,7 +40,7 @@ class ClientPermission(BasePermission):
             if obj in Client.objects.filter(id=obj.id, client_event__support_contact=request.user):
                 return True
         if user_role == 'SALES' and request.method in ['GET', 'PUT']:
-            if obj in Client.objects.filter(id=obj.id, client_contract__sales_contact=request.user):
+            if obj in Client.objects.filter(id=obj.id, sales_contact_id=request.user):
                 return True
         return False
 
@@ -52,7 +52,7 @@ class ContractPermission(BasePermission):
         Allow users in the Sales team to perform a GET and a POST.
         """
         user_role = request.user.role
-        if user_role == 'MANAGEMENT' and request.method == 'GET':
+        if user_role == 'MANAGEMENT' and request.method in ['GET', 'PUT']:
             return True
         if user_role == 'SALES':
             return True
@@ -79,11 +79,11 @@ class EventPermission(BasePermission):
         Allow users in the Sales team to perform a GET and a POST.
         """
         user_role = request.user.role
-        if user_role == 'MANAGEMENT' and request.method == 'GET':
+        if user_role == 'MANAGEMENT' and request.method in ['GET', 'PUT']:
             return True
-        if user_role == 'SUPPORT' and request.method == 'GET':
+        if user_role == 'SUPPORT' and request.method in ['GET', 'PUT']:
             return True
-        if user_role == 'SALES':
+        if user_role == 'SALES' and request.method == 'POST':
             return True
         return False
 
@@ -95,6 +95,9 @@ class EventPermission(BasePermission):
         user_role = request.user.role
         if user_role == 'MANAGEMENT' and request.method in ['GET', 'PUT']:
             return True
-        if user_role == 'SUPPORT' and obj.support_contact == request.user and request.method in ['GET', 'PUT']:
-            return True
+        if user_role == 'SUPPORT' and obj.support_contact == request.user:
+            if request.method == 'GET':
+                return True
+            if request.method == 'PUT' and obj.event_status != 'FINISHED':
+                return True
         return False
